2a6dbacd1e47a04aefd7c91712481b4fa7904125
VEBoris <Visuya@outlook.fr>
2021-11-22 17:32:27 +0100

    add sauce

--- back_end/app.js
+++ back_end/app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require('body-parser');
+const path = require('path');
 
+const sauceRoutes = require('./routes/sauce');
 const userRoutes = require('./routes/user');
 
 mongoose.connect('mongodb+srv://visuya:visuyavisu@cluster0.td0fa.mongodb.net/test?retryWrites=true&w=majority',
@@ -21,7 +23,9 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 
-// app.use('/api/stuff', stuffRoutes);
+app.use('/images', express.static(path.join(__dirname, 'images'))); 
+
+app.use('/api/sauces', sauceRoutes);
 app.use('/api/auth', userRoutes);
 
 module.exports = app;
